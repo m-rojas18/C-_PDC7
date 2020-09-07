c24988c855622290277709cc56f02e31475bf010
Miguel Rojas <miguel@Miguels-MacBook-Air.local>
2020-09-06 18:13:24 -0600

    Actualizar Programa Herencia

---
+++ Herencia/Maestro.cpp
@@ -0,0 +1,11 @@
+#include "Persona.hpp"
+#include "Maestro.hpp"
+
+
+Maestro::Maestro(string Nombre, string Apellido, Persona::TipoGenero Genero, string NumeroTalentoHumano) :Persona(Nombre,Apellido,Genero){
+    this->NumeroTalentoHumano = NumeroTalentoHumano;
+}
+
+string Maestro::obtenerNumero(){
+    return NumeroTalentoHumano;
+}
